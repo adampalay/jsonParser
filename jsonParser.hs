25e86178476b9f6d24980a2e98ac6080d075d070
Adam Palay <adam@edx.org>
2016-09-30 15:58:43 -0400

    make parser a datatype with Error constructor

--- jsonParser.hs
+++ jsonParser.hs
@@ -3,7 +3,10 @@
 data JSON = N Double | S String | B Bool | Null | L [JSON] | O [(String, JSON)]
     deriving (Show, Eq)
 
-type Parser a = (a, String)
+-- type Parser a = (a, String)
+
+data Parser a = Success a String | Error
+    deriving Show
 
 -- O [("key", I 1)]
 
@@ -11,9 +14,9 @@ type Parser a = (a, String)
 
 -- starting from first character, it can start with {, [, ", 0-9, -
 
-topParse :: String -> JSON
-topParse s = if eatWhitespace rest == "" then json else error "didn't work"
-    where (json, rest) = parse s
+topParse :: String -> Parser JSON
+topParse s = if eatWhitespace rest == "" then Success json "" else Error
+    where Success json rest = parse s
 
 parse :: String -> Parser JSON
 parse s@('{':_) = parseObject s
@@ -26,81 +29,85 @@ parse s@('n':_) = parseNull s
 parse (' ':xs) = parse xs
 parse ('\n':xs) = parse xs
 parse ('\t':xs) = parse xs
-parse s = parseNumber s
+-- parse s = parseNumber s
+parse _ = Error
 
 parseNull :: String -> Parser JSON
-parseNull ('n':'u':'l':'l':xs) = (Null, xs)
+parseNull ('n':'u':'l':'l':xs) = Success Null xs
+parseNull _ = Error
 
 parseBool :: String -> Parser JSON
-parseBool ('t':'r':'u':'e':xs) = (B True, xs)
-parseBool ('f':'a':'l':'s':'e':xs) = (B False, xs)
+parseBool ('t':'r':'u':'e':xs) = Success (B True) xs
+parseBool ('f':'a':'l':'s':'e':xs) = Success (B False) xs
+parseBool _ = Error
 
 parseString :: String -> Parser JSON
-parseString ('"':xs) = (S body, xs')
-    where (body, ('"':xs')) = parseBody xs
+parseString ('"':xs) = Success (S body) xs'
+    where Success body ('"':xs') = parseBody xs
+parseString _ = Error
 
 parseBody :: String -> Parser String
-parseBody s@('"':xs) = ("", s)
-parseBody ('\\':'n':xs) = ('\n':body, xs')
-    where (body, xs') = parseBody xs
-parseBody ('\\':'t':xs) = ('\t':body, xs')
-    where (body, xs') = parseBody xs
-parseBody ('\\':'\\':xs) = ('\\':body, xs)
-    where (body, xs') = parseBody xs
-parseBody ('\\':'"':xs) = ('"':body, xs)
-    where (body, xs') = parseBody xs
-parseBody ('\\':c:xs) = (c:body, xs)
-    where (body, xs') = parseBody xs
-parseBody (x:xs) = (x:body, xs')
-    where (body, xs') = parseBody xs
+parseBody s@('"':xs) = Success "" s
+parseBody ('\\':'n':xs) = Success ('\n':body) xs'
+    where Success body xs' = parseBody xs
+parseBody ('\\':'t':xs) = Success ('\t':body) xs'
+    where Success body xs' = parseBody xs
+parseBody ('\\':'\\':xs) = Success ('\\':body) xs
+    where Success body xs' = parseBody xs
+parseBody ('\\':'"':xs) = Success ('"':body) xs
+    where Success body xs' = parseBody xs
+parseBody ('\\':c:xs) = Success (c:body) xs
+    where Success body xs' = parseBody xs
+parseBody (x:xs) = Success (x:body) xs'
+    where Success body xs' = parseBody xs
 
 parseList :: String -> Parser JSON
-parseList ('[':xs) = (L items, xs')
-    where (items, ']':xs') = parseListItems xs
+parseList ('[':xs) = Success (L items) xs'
+    where Success items (']':xs') = parseListItems xs
 
 parseListItems :: String -> Parser [JSON]
 parseListItems (' ':xs) = parseListItems xs -- still looking for an item
 parseListItems ('\n':xs) = parseListItems xs -- still looking for an item
-parseListItems s = (item:items, xs')
+parseListItems s = Success (item:items) xs'
     where
-       (item, xs) = parse s
-       (items, xs') = parseListNext xs
+       Success item xs = parse s
+       Success items xs' = parseListNext xs
 
 parseListNext :: String -> Parser [JSON]
-parseListNext s@(']':xs) = ([], s)  -- no next item, the list is done
+parseListNext s@(']':xs) = Success [] s  -- no next item, the list is done
 parseListNext (',':xs) = parseListItems xs -- parse the next item
 parseListNext (' ':xs) = parseListNext xs -- still looking for a separator
 parseListNext ('\n':xs) = parseListNext xs -- still looking for a separator
 
 parseObject :: String -> Parser JSON
-parseObject ('{':xs) = (O pairs, xs')
-    where (pairs, '}':xs') = parseObjectPairs xs
+parseObject ('{':xs) = Success (O pairs) xs'
+    where Success pairs ('}':xs') = parseObjectPairs xs
 
 parseObjectPairs :: String -> Parser [(String, JSON)]
 parseObjectPairs (' ':xs) = parseObjectPairs xs
 parseObjectPairs ('\n':xs) = parseObjectPairs xs
 parseObjectPairs ('\t':xs) = parseObjectPairs xs
-parseObjectPairs s = (pair:pairs, xs')
+parseObjectPairs s = Success (pair:pairs) xs'
     where
-        (pair, xs) = parsePair s
-        (pairs, xs') = parsePairNext xs
+        Success pair xs = parsePair s
+        Success pairs xs' = parsePairNext xs
 
 parsePair :: String -> Parser (String, JSON)
-parsePair s = ((key, value), xsFinal)
+parsePair s = Success (key, value) xsFinal
     where
-        (S key, xs) = parseString s
+        Success (S key) xs = parseString s
         xs' = (eatWhitespace . (eat ':') . eatWhitespace) xs
-        (value, xsFinal) = parse xs'
+        Success value xsFinal = parse xs'
 
 parsePairNext :: String -> Parser [(String, JSON)]
-parsePairNext s@('}':xs) = ([], s)
+parsePairNext s@('}':xs) = Success [] s
 parsePairNext (' ':xs) = parsePairNext xs
 parsePairNext ('\n':xs) = parsePairNext xs
 parsePairNext ('\t':xs) = parsePairNext xs
-parsePairNext s = (pairs, xs)
+parsePairNext s = Success pairs xs
     where
         s' = (eatWhitespace . (eat ',') . eatWhitespace) s
-        (pairs, xs) = parseObjectPairs s'
+        Success pairs xs = parseObjectPairs s'
 
 eat :: Char -> String -> String
 eat c (x:xs) | x == c = xs
